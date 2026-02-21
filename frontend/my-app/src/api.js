39b59ce8c5ac187feac86932ebe2cef17a8eec1d
Roshan sharma <90971534+theroshancool@users.noreply.github.com>
2026-02-21 16:19:08 +0530

    Update api.js

--- frontend/my-app/src/api.js
+++ frontend/my-app/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
     // baseURL: "http://localhost:3000",
-    baseURL: "baseURL: "http://localhost:3000",",
+       baseURL: "https://micro-market-nh0l.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
