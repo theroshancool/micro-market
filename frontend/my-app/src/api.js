7cf9ddecd4a920212691a8ea0dee61b936911274
Roshan sharma <90971534+theroshancool@users.noreply.github.com>
2026-02-21 15:59:48 +0530

    Update api.js

--- frontend/my-app/src/api.js
+++ frontend/my-app/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:3000",
+    // baseURL: "http://localhost:3000",
+    baseURL: "baseURL: "http://localhost:3000",",
 });
 
 API.interceptors.request.use((req) => {
